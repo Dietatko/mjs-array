c909ed683eee5e5ada09835791c6827b00fac093
Andrej Burger <burgera@impruvit.net>
2019-09-08 18:00:49 +0200

    Minification added as a NPM build script

--- mjs_fs/mjs_array.js
+++ mjs_fs/mjs_array.js
@@ -1,15 +1,1 @@
-let Array={create:function(arr){if(arr===undefined){arr=[]}
-if(typeof arr!=="array"){return null}
-arr.__p=this._prot;return arr},from:function(arr,mapFn,me){if(typeof arr!=="array"){return null}
-if(mapFn!==undefined&&typeof arr!=="function"){return null}
-let dst=Array.create();let len=this.length;if(mapFn){for(let i=0;i<len;i++){dst.push(func.apply(me,[this[i],i,this]))}}else{for(let i=0;i<len;i++){dst.push(this[i])}}
-return dst},_prot:{every:function(func,me){let len=this.length;for(let i=0;i<len;i++){if(!func.apply(me,[this[i],i,this])){return!1}}
-return!0},filter:function(func,me){let dst=Array.create();let len=this.length;for(let i=0;i<len;i++){let e=this[i];if(func.apply(me,[e,i,this])){dst.push(e)}}
-return dst},find:function(func,me){let len=this.length;for(let i=0;i<len;i++){let e=this[i];if(func.apply(me,[e,i,this])){return e}}
-return undefined},findIndex:function(func,me){let len=this.length;for(let i=0;i<len;i++){if(func.apply(me,[this[i],i,this])){return i}}
-return-1},forEach:function(func,me){let len=this.length;for(let i=0;i<len;i++){func.apply(me,[array[i],i,this])}},map:function(func,me){let dst=Array.create();let len=this.length;for(let i=0;i<len;i++){dst.push(func.apply(me,[this[i],i,this]))}
-return dst},reduce:function(func,acc){let len=this.length;let i=0;if(acc===undefined){if(len<1){return undefined}
-acc=this[0];i++}
-for(i=i;i<len;i++){acc=func(acc,thia[i],i,this)}
-return acc},some:function(func,me){let len=this.length;for(let i=0;i<len;i++){if(func.apply(me,[this[i],i,this])){return!0}}
-return!1},}}
+let Array={create:function(t){if(t===undefined){t=[]}if(typeof t!=="array"){return null}t.__p=this._prot;return t},from:function(t,e,r){if(typeof t!=="array"){return null}if(e!==undefined&&typeof t!=="function"){return null}let n=Array.create();let i=this.length;if(e){for(let t=0;t<i;t++){n.push(func.apply(r,[this[t],t,this]))}}else{for(let t=0;t<i;t++){n.push(this[t])}}return n},_prot:{every:function(t,e){let r=this.length;for(let n=0;n<r;n++){if(!t.apply(e,[this[n],n,this])){return false}}return true},filter:function(t,e){let r=Array.create();let n=this.length;for(let i=0;i<n;i++){let n=this[i];if(t.apply(e,[n,i,this])){r.push(n)}}return r},find:function(t,e){let r=this.length;for(let n=0;n<r;n++){let r=this[n];if(t.apply(e,[r,n,this])){return r}}return undefined},findIndex:function(t,e){let r=this.length;for(let n=0;n<r;n++){if(t.apply(e,[this[n],n,this])){return n}}return-1},forEach:function(t,e){let r=this.length;for(let n=0;n<r;n++){t.apply(e,[array[n],n,this])}},map:function(t,e){let r=Array.create();let n=this.length;for(let i=0;i<n;i++){r.push(t.apply(e,[this[i],i,this]))}return r},reduce:function(t,e){let r=this.length;let n=0;if(e===undefined){if(r<1){return undefined}e=this[0];n++}for(n=n;n<r;n++){e=t(e,thia[n],n,this)}return e},some:function(t,e){let r=this.length;for(let n=0;n<r;n++){if(t.apply(e,[this[n],n,this])){return true}}return false}}};
